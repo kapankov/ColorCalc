56e6112d192271086b419884b63a3a7743a83108
kapankov <explorus@mail.ru>
2020-09-10 09:59:57 +0500

    stdcin_inrange function template

--- ColorCalc.cpp
+++ ColorCalc.cpp
@@ -4,129 +4,74 @@
 using namespace COLORNS;
 using namespace std;
 
+template <typename T>
+T stdcin_inrange (std::string message, T low, T up)
+{
+    T value = 0;
+    while ((std::cout << message)
+        && (!(std::cin >> value) || value > up || value < low)) {
+        std::cout << "That's not a number between ";
+        std::cout << low;
+        std::cout << " and ";
+        std::cout << up;
+        std::cout << ";\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    return value;
+};
+
+
 int main()
 {
     int i = 0;
     double ch1 = -1.0;
     double ch2 = -1.0;
     double ch3 = -1.0;
-    for (;;) {
-        cout << "Please, select input color model (1 - RGB, 2 - HSV, 3 - XYZ, 4 - Lab): ";
-        if ((cin >> i) && (i>0 && i<5))
-        {
-            cin.clear();
-            break;
-        }
-        else
-        {
-            cout << "Please enter a valid integer (1 to 4)" << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-    }
+    Color clr;
 
-    for (;;) {
-        if (i == 1)
-            cout << "Please, enter red (0..1.0): ";
-        else if (i == 2)
-            cout << "Please, enter hue (0..360): ";
-        else if (i == 3)
-            cout << "Please, enter X (0..1.0): ";
-        else
-            cout << "Please, enter L (0..100.0): ";
+    i = stdcin_inrange<int>("Please, select input color model (1 - RGB, 2 - HSV, 3 - XYZ, 4 - Lab): ", 1, 4);
 
-        cin >> ch1;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        if (!cin.fail() && (i==1?ch1>=0 && ch1<=1.0:ch1>=0 && ch1<=360.0))
-            break;
-        else
-        {
-            if (i == 2)
-                cout << "Please enter a valid double value (0..360.0)" << endl;
-            if (i == 4)
-                cout << "Please enter a valid double value (0..100.0)" << endl;
-            else
-                cout << "Please enter a valid double value (0..1.0)" << endl;
-            cin.clear();
-        }
-    }
-
-    for (;;) {
-        if (i == 1)
-            cout << "Please, enter green (0..1.0): ";
-        else if (i == 2)
-            cout << "Please, enter saturation (0..1.0): ";
-        else if (i == 3)
-            cout << "Please, enter Y (0..1.0): ";
-        else
-            cout << "Please, enter a (-100..100.0): ";
-
-        cin >> ch2;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        if (!cin.fail() && (ch2>=0 && ch2<=1.0))
-            break;
-        else
-        {
-            if (i == 4)
-                cout << "Please enter a valid double value (-100.0..100.0)" << endl;
-            else
-                cout << "Please enter a valid double value (0..1.0)" << endl;
-            cin.clear();
-        }
-    }
-
-    for (;;) {
-        if (i==1)
-            cout << "Please, enter blue value (0..1.0): ";
-        else if (i == 1)
-            cout << "Please, enter value (0..1.0): ";
-        else if (i == 1)
-            cout << "Please, enter Z (0..1.0): ";
-        else
-            cout << "Please, enter b (-100.0..100.0): ";
-
-        cin >> ch3;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        if (!cin.fail() && (ch3>=0 && ch3<=1.0))
-            break;
-        else
-        {
-            if (i == 4)
-                cout << "Please enter a valid double value (-100.0..100.0)" << endl;
-            else
-                cout << "Please enter a valid double value (0..1.0)" << endl;
-            cin.clear();
-        }
-    }
-
-    if (i==1)
+    switch (i)
     {
-        Color clr(RgbColor(ch1, ch2, ch3));
+    case 1:
+        ch1 = stdcin_inrange<double>("Please, enter red (0..1.0): ", 0.0, 1.0);
+        ch2 = stdcin_inrange<double>("Please, enter green (0..1.0): ", 0.0, 1.0);
+        ch3 = stdcin_inrange<double>("Please, enter blue (0..1.0): ", 0.0, 1.0);
+        clr = RgbColor(ch1, ch2, ch3);
         cout << clr.GetHSV() << ", lightness:" << clr.GetHSV().GetLightness() << ", relative luminance:" << clr.GetHSV().GetLuminance() << endl;
         cout << clr.GetXYZ() << endl;
         cout << clr.GetLAB() << endl;
-    }
-    else if (i == 2)
-    {
-        Color clr(HsvColor(ch1, ch2, ch3));
+        break;
+    case 2:
+        ch1 = stdcin_inrange<double>("Please, enter hue (0..360): ", 0.0, 360.0);
+        ch2 = stdcin_inrange<double>("Please, enter saturation (0..1.0): ", 0.0, 1.0);
+        ch3 = stdcin_inrange<double>("Please, enter brightness value (0..1.0): ", 0.0, 1.0);
+        clr = HsvColor(ch1, ch2, ch3);
         cout << "lightness:" << clr.GetHSV().GetLightness() << ", relative luminance:" << clr.GetHSV().GetLuminance() << endl;
         cout << clr.GetRGB() << endl;
         cout << clr.GetXYZ() << endl;
         cout << clr.GetLAB() << endl;
-    }
-    else if (i == 3)
-    {
-        Color clr(XyzColor(ch1, ch2, ch3));
+        break;
+    case 3:
+        ch1 = stdcin_inrange<double>("Please, enter X (0..1.0): ", 0.0, 1.0);
+        ch2 = stdcin_inrange<double>("Please, enter Y (0..1.0): ", 0.0, 1.0);
+        ch3 = stdcin_inrange<double>("Please, enter Z (0..1.0): ", 0.0, 1.0);
+        clr = XyzColor(ch1, ch2, ch3);
         cout << clr.GetRGB() << endl;
         cout << clr.GetHSV() << ", lightness:" << clr.GetHSV().GetLightness() << ", relative luminance:" << clr.GetHSV().GetLuminance() << endl;
         cout << clr.GetLAB() << endl;
-    }
-    else
-    {
-        Color clr(LabColor(ch1, ch2, ch3));
+        break;
+    case 4:
+        ch1 = stdcin_inrange<double>("Please, enter L (0..100.0): ", 0.0, 100.0);
+        ch2 = stdcin_inrange<double>("Please, enter a (0..100.0): ", 0.0, 100.0);
+        ch3 = stdcin_inrange<double>("Please, enter b (0..100.0): ", 0.0, 100.0);
+        clr = LabColor(ch1, ch2, ch3);
         cout << clr.GetXYZ() << endl;
         cout << clr.GetRGB() << endl;
         cout << clr.GetHSV() << ", lightness:" << clr.GetHSV().GetLightness() << ", relative luminance:" << clr.GetHSV().GetLuminance() << endl;
+        break;
+    default:
+        return 0;
     }
-
 }
